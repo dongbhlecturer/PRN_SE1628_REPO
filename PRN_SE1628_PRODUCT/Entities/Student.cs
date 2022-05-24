9d1f83b597f9fdfd164343bf020ad1851380dd55
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-24 10:13:30 +0700

    commit

---
+++ PRN_SE1628_PRODUCT/Entities/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Prn.Se1628;
+public class Student:IComparable<Student>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Mark { get; set; }
+    public Student()
+    {
+
+    }
+
+    public Student(int id, string name, double mark)
+    {
+        Id = id;
+        Name = name;
+        Mark = mark;
+    }
+
+    public override string? ToString() => $"[Id = {Id}, Name ={Name} and Mark = {Mark}]";
+
+    public int CompareTo(Student? o)
+    {
+        //0: this vs o = nhau, 1 this > o else 
+        int result = 0;
+        if(o is not null)
+        {
+            result =  o.Mark.CompareTo(this.Mark);
+            //neu trung nhau diem thi so sanh den ten
+        }
+        return result;
+    }
+}
---
+++ PRN_SE1628_PRODUCT/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using static System.Console;
+using Prn.Se1628;
+
+
+Student[] students = new Student[] {
+new Student(1,"Dong",8.5d),
+new Student(2, "An", 3.5d),
+new Student(3, "Thang", 9.5d),
+new Student(4, "Hai", 4.5d)
+};
+foreach (Student s in students)
+{
+    WriteLine(s);
+}
+
+Array.Sort<Student>(students);
+Console.WriteLine("------------------------");
+
+
+
+foreach (Student s in students)
+{
+    WriteLine(s);
+}
+ReadLine();
